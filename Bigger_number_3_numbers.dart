c1133f68c6aa40cca9b9756896e55fa864d25838
galvered <93703821+galvered@users.noreply.github.com>
2021-11-04 10:42:42 +0200

    Create Bigger_number_3_numbers.dart

---
+++ Bigger_number_3_numbers.dart
@@ -0,0 +1,27 @@
+void main() {
+  int number = 20;
+  int numberTwo = 30;
+  int numberThree = 10;
+  
+  if (number > numberTwo) {
+    if (number > numberThree) {
+      print ('$number is bigger');
+     
+    }
+    else {
+      print ('$numberThree is bigger');
+      
+    }
+  }
+  else {
+    if (numberTwo > numberThree){
+      print ('$numberTwo is bigger');
+    }
+    else {
+      print ('$numberThree is bigger');
+    }
+  }
+  
+ 
+ 
+}
