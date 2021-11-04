11850fc1a88e4efddfef123dc09c19a636d2aae0
galvered <93703821+galvered@users.noreply.github.com>
2021-11-04 10:11:39 +0200

    Create even_odd.dart

---
+++ even_odd.dart
@@ -0,0 +1,31 @@
+void main() {
+  // Program to check if the given number is Even or Odd.
+  
+  int number = 12;
+  
+  // Conditional Statements - if, if else
+  
+  // After checking even , I want to check if the number is 10 or not?
+  
+  
+  
+  // Single Time = means assignment
+  // Two Times == means comparision.
+  
+  
+  if(number % 2 == 0 ) {
+    print('The Number is Even Number');
+    
+    if(number == 10) {
+      print('The number is 10');
+    }
+    
+    else {
+      print('Number is not 10');
+    }
+  }
+  else {
+    print('The Number is Odd Number');
+  }
+ 
+}
